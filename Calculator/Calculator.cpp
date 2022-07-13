ef2d2add78f72b9e7c1a83de39444d6e519c561e
Artem Karpuk <75688693+aARTEMkad@users.noreply.github.com>
2022-07-13 15:31:53 +0300

    Update Calculator.cpp

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,57 +1,27 @@
-﻿
 #include <iostream>
 
 int main()
 {
     int menu;
-    double number_one, number_two, number_three, number_flour, suma = 0;
-    char symvol,symvol_1;
-    std::cout << "Write the number of numbers for the calculator\n";
-    std::cin >> menu;
+
+    double  suma = 0; //reply
+
+    double number_one = 0, number_two = 0, number_three = 0;
+
+    char symvol, symvol_1;
+
+    std::cout << "Write the number of numbers for the calculator:";
+    std::cin >> menu; //selects how many numbers will be in the expression
+
     switch (menu)
     {
     case 2:
-        std::cout << "Write Expression:";
+        std::cout << "Write expression through a space:";
         std::cin >> number_one;
 
         std::cin >> symvol;
 
         std::cin >> number_two;
-        if (symvol == '+') {
-            suma = number_one + number_two;
-           
-        }
-        else if (symvol == '-') {
-             suma = number_one - number_two;
-           
-        }
-        else if (symvol == '*') {
-             suma = number_one * number_two;
-          
-        }
-        else if (symvol == '/') {
-            suma = number_one / number_two;
-          
-        }
-        std::cout << "Reply = " << suma;
-        break;
-    case 3:
-        std::cout << "Write Expression:";
-        std::cin >> number_one;
-
-        std::cin >> symvol;
-
-        std::cin >> number_two;
-
-        std::cin >> symvol_1;
-
-        std::cin >> number_three;
-        if (symvol > symvol_1) {
-            std::cout << "sumvol > symvol_1\n";
-            std::cout << int(symvol) << std::endl;
-            std::cout << int(symvol_1) << std::endl;
-        }
-        else std::cout << "symvol>symvol_1\n" << symvol << "\n" << symvol_1;
         if (symvol == '+') {
             suma = number_one + number_two;
 
@@ -70,28 +40,81 @@ int main()
         }
         std::cout << "Reply = " << suma;
         break;
-    case 4:
+    case 3:
+        std::cout << "Write expression through a space:";
+        std::cin >> number_one;
 
+        std::cin >> symvol;
+
+        std::cin >> number_two;
+
+        std::cin >> symvol_1;
+
+        std::cin >> number_three;
+
+        if ((int)symvol_1 == 42 || (int)symvol_1 == 47) {  //checks whether the second character is * or /
+            if (symvol_1 == '*') {
+                suma = number_two * number_three;
+
+            }
+            else if (symvol_1 == '/') {
+                suma = number_two * number_three;
+            }
+            if (symvol == '+') {
+                suma += number_one;
+
+            }
+            else if (symvol == '-') {
+                suma -= number_one;
+
+            }
+            else if (symvol == '*') {
+                suma *= number_one;
+
+            }
+            else if (symvol == '/') {
+                suma /= number_one;
+
+            }
+        }
+
+
+
+
+        else {
+            if (symvol == '+') {
+                suma = number_one + number_two;
+
+
+            }
+            else if (symvol == '-') {
+                suma = number_one - number_two;
+
+            }
+            else if (symvol == '*') {
+                suma = number_one * number_two;
+
+            }
+            else if (symvol == '/') {
+                suma = number_one / number_two;
+            }
+
+            if (symvol_1 == '+') {
+                suma += number_three;
+
+            }
+            else if (symvol_1 == '-') {
+                suma -= number_three;
+
+            }
+        }
+
+        std::cout << "Reply = " << suma;
         break;
     default:
+        std::cout << "Soon....";
         break;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
